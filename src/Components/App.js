45a4f0d1ba5abfba95018fbaba2d1b7737b5dda4
ntaparia <41445360+ntaparia@users.noreply.github.com>
2022-05-12 17:12:08 -0400

    Add files via upload

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import VariablePage from "./VariablePage";
+import { setAccounts } from "../actions";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    componentDidMount() {
+        this.getData();
+    }
+    getData(){
+        axios.get('https://my-json-server.typicode.com/bnissen24/project2DB/accounts')
+            .then(response => {
+                this.props.setAccounts(response.data);
+            }).catch(error => {
+                this.props.tasksError();
+        });
+    }
+    render(){
+        const { view } = this.state;
+        switch (view) {
+            case 'accounts':
+                return (this.wrapPage(<Page1 />));
+            case 'transactions':
+                return (this.wrapPage(<Page2 />));
+            case 'newaccount':
+                return (this.wrapPage(<Page3/>));
+            default:
+                return (this.wrapPage(<h2>Please make a valid selection</h2>));
+        }
+    }
 }
 
-export default App;

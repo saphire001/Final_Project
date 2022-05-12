67c3be3ed6fcf2172487d9f9a61642ace1f34695
ntaparia <41445360+ntaparia@users.noreply.github.com>
2022-05-12 17:21:16 -0400

    Add files via upload

---
+++ src/Components/VariablePage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class VariablePage extends React.Component {
+
+    goToHome = () => {
+        this.props.history.push(`/`);
+    }
+
+    render() {
+        console.log(this.props.history)
+        return (
+            <div className="page-variable">
+                <h1>You Clicked on { this.props.match.params.id }</h1>
+                <button onClick={this.goToHome}>Go To Home</button>
+            </div>
+        );
+    }
+}
+
+export default VariablePage;

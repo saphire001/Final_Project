53e68a0617802ad647ffae815a28fe196db470dc
saanikajoshi <spj29@njit.edu>
2022-05-12 18:01:58 -0400

    Transaction created

---
+++ src/Components/TransactFormatPage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { removeAccount } from "../actions";
+import TransactFormatPage from "./TransactFormatPage";
+
+class TransactList extends React.Component {
+    render(){
+        const userTransacts = this.props.transacts.map(transaction  => {
+            return <TransactFormatPage transaction={transaction} key={transaction.id}/>
+        });
+        return(
+            <ul className = "task-list list-group">
+                { userTransacts }
+            </ul>
+        )
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        transacts : state.transacts
+    };
+}
+export default connect(mapStateToProps)(TransactList);

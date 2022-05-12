ec6cf211cabef1296a1a3ac76abdb2a1b54a50f0
saanikajoshi <spj29@njit.edu>
2022-05-12 18:18:37 -0400

    Pages Added

---
+++ src/Components/Page1.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Page1 = props => {
+    return (
+        <div className="page-one">
+            <h1>All Accounts</h1>
+
+        </div>
+
+    )
+}
+
+export default Page1;

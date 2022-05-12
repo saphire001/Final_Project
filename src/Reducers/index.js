8f662fc513e54dfcc5b6abb2a0a1f5c590596951
ntaparia <41445360+ntaparia@users.noreply.github.com>
2022-05-12 18:35:41 -0400

    Add files via upload

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
-
-import userChoiceReducer from './userChoiceReducer';
+import errorReducer from './errorReducer';
+import accountsReducer from './accountsReducer';
 
 export default combineReducers( {
-    tasks: userChoiceReducer
+
+    accounts: accountsReducer,
+    errors: errorReducer
+
 });

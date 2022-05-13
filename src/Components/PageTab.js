39ffdfdb052247f4fa8b673958ddb5711ee74c1d
saanikajoshi <spj29@njit.edu>
2022-05-12 23:02:35 -0400

    error fixing but wite showing

--- src/Components/PageTab.js
+++ src/Components/PageTab.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import './PageTabs.css';
+import './PageTab.css';
 
 class PageTabs extends React.Component {
     state = { currentPage: '/' }

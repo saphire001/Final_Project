2e4693709bca8e45d45d9f0e943ff6f3d1d93ab1
ntaparia <41445360+ntaparia@users.noreply.github.com>
2022-05-13 12:09:07 -0400

    change index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
-import App from './components/App';
-import reducers from './reducers';
+import App from './Components/App';
+import reducers from './Reducers';
 
 ReactDOM.render(
-    <Provider store = {createStore(reducers)}>
-        <App />
-    </Provider>,
-    document.getElementById('root')
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
 );

911a59aeb4b4c20a150c841d8d3e01eee3f7bd6f
saanikajoshi <spj29@njit.edu>
2022-05-13 11:15:09 -0400

    footer added

---
+++ src/Components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./Footer.css";
+
+class Footer extends React.Component {
+
+  render() {
+    return (
+      <footer class="footer">
+        <div class="container">
+          <span>Justin Agustin &amp; Ibrahim Hamza &copy; 2021 NJIT</span>
+        </div>
+      </footer>
+    )
+  }
+
+};
+
+export default Footer;

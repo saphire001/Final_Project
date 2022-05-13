aa9626479f3453ffc92507b4dc5cc957f5adcb2a
ntaparia <41445360+ntaparia@users.noreply.github.com>
2022-05-13 16:27:06 -0400

    Update index.js

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,8 +1,18 @@
 import { combineReducers } from 'redux';
+
+//import userChoiceReducer from './userChoiceReducer';
+import errorReducer from './errorReducer';
+//import accountsReducer from './accountsReducer';
 import accountsReducer from './accountsReducer';
+
 import transactsReducer from './transactsReducer';
 
-export default combineReducers({
-  accounts: accountsReducer,
-  transactions: transactsReducer
+export default combineReducers( {
+
+    //tasks: userChoiceReducer,
+    //accounts: accountsReducer,
+    accounts: accountsReducer,
+    transacts: transactsReducer,
+    errors: errorReducer
+
 });

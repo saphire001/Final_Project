48d2cbfc3547e484b50843adca04c6850cfdf89d
saanikajoshi <spj29@njit.edu>
2022-05-13 13:38:32 -0400

    Merge branch 'main' of https://github.com/ntaparia/Final_Project

--- src/Components/Transactions.js
+++ src/Components/Transactions.js
@@ -5,23 +5,23 @@ const Transactions = props => {
       <div class="card" style={{width:"400px;", position:"relative;", margin:"50px;"}}>
           <div class="card-body">
               <h5 class="card-title">
-                    ID of Transaction: {props.transaction._id}
+                    Transaction ID: {props.transactions._id}
               </h5>
 
               <p class="card-test">
-                   ID of Account: {props.transaction.accountId}
+                  Account ID: {props.transactions.accountId}
               </p>
 
               <p class="card-text">
-                  Memo: {props.transaction.name}
+                  Memo: {props.transactions.name}
               </p>
 
               <p class="card-text">
-                  Debit or Credit: {props.transaction.type}
+                  Type: {props.transactions.type}
               </p>
 
               <p class="card-text">
-                  Amount: ${ props.transaction.amount}
+                  Amount: ${ props.transactions.amount}
               </p>
           </div>
       </div>

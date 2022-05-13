070442523288ef4b5fea69736041bb16946645bd
ntaparia <41445360+ntaparia@users.noreply.github.com>
2022-05-13 16:19:59 -0400

    Update Page1.js

--- src/Components/Page1.js
+++ src/Components/Page1.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import AccountList from './AccountList';
-import Footer from './Footer';
+//import { Link } from 'react-router-dom';
 
+//import factionCard from "./AccountsList";
+
+/****
+ need to pass this:
+ 1. name of faction
+ 2. balance
+ 3. id?
+ 4. the list of accounts
+ 5. description
+ *can probably display two cards per row
+ *
+ ****/
 const Page1 = props => {
-  return (
-    <div className="page-one">
-      <AccountList />
-      <Footer />
-    </div>
-  )
+    return (
+        <div className="page-one">
+            <h1>All Accounts</h1>
+
+        </div>
+
+    )
 }
 
 export default Page1;

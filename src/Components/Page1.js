b3e12d00919e420b23a5b3521f9d19bfb9fdeddb
ntaparia <41445360+ntaparia@users.noreply.github.com>
2022-05-13 11:03:11 -0400

    Add files via upload

--- src/Components/Page1.js
+++ src/Components/Page1.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import AccountList from './AccountList';
+import Footer from './Footer';
 
 const Page1 = props => {
-    return (
-        <div className="page-one">
-            <h1>All Accounts</h1>
-
-        </div>
-
-    )
+  return (
+    <div className="page-one">
+      <AccountList />
+      <Footer />
+    </div>
+  )
 }
 
 export default Page1;

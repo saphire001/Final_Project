a642530357fad67e47a3a5ec322c6ab9218f903d
ntaparia <41445360+ntaparia@users.noreply.github.com>
2022-05-13 12:11:01 -0400

    changes to Reducer folder

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,16 +1,8 @@
 import { combineReducers } from 'redux';
-
-
-import errorReducer from './errorReducer';
-
 import accountsReducer from './accountsReducer';
-
 import transactsReducer from './transactsReducer';
 
-export default combineReducers( {
-
-    accounts: accountsReducer,
-    transacts: transactsReducer,
-    errors: errorReducer
-
+export default combineReducers({
+  accounts: accountsReducer,
+  transactions: transactsReducer
 });

0869575dce272ffcba53606af9086ac4927c7eb1
ntaparia <41445360+ntaparia@users.noreply.github.com>
2022-05-13 16:20:55 -0400

    Update Page3.js

--- src/Components/Page3.js
+++ src/Components/Page3.js
@@ -7,10 +7,10 @@ const Page3 = props => {
             <h3>Register Account</h3>
             <ul>
 
-                <li><Link to={'page/1'}>Page 1</Link></li>              
-                <li><Link to={'page/2'}>Page 2</Link></li>              
+                <li><Link to={'page/1'}>Page 1</Link></li>              {/****to={'/page1'}: look like normal links ***/}
+                <li><Link to={'page/2'}>Page 2</Link></li>              {/*****component does not need to know if these are variable routes****/}
                 <li><Link to={'page/3'}>Page 3</Link></li>
-                <li><Link to={'page/4'}>Page 4</Link></li>              
+                <li><Link to={'page/4'}>Page 4</Link></li>              {/*****for the component that makes the link**********/}
 
             </ul>
         </div>

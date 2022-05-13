e12732552a18e0c5f8f09e36762edb21f44eb93d
ntaparia <41445360+ntaparia@users.noreply.github.com>
2022-05-13 16:21:48 -0400

    Update PageTabs.js

--- src/Components/PageTabs.js
+++ src/Components/PageTabs.js
@@ -1,47 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import './PageTabs.css';
+
 class PageTabs extends React.Component {
-  state = { currentPage: '/' }
+    state = { currentPage: '/' }
 
-  isActiveTab(tabName) {
-    return (tabName === this.state.currentPage) ? 'nav-link active' : 'nav-link';
-  }
+    isActiveTab(tabName) {
+        return (tabName === this.state.currentPage) ? 'nav-link active' : 'nav-link';
+    }
 
-  onTabClick(event, tabName) {
-    this.setState({ currentPage: tabName })
-  }
+    onTabClick(event, tabName) {
+        this.setState({ currentPage: tabName })
+    }
 
-  render() {
-    return (
-      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <a class="navbar-brand" href="/">
-        Westorsi Bank
-        </a>
-
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-          <ul class="navbar-nav mr-auto" style={{ textAlign: 'center' }}>
-            <li class="nav-item">
-              <Link className={this.isActiveTab('/')} to="/" onClick={event => this.onTabClick(event, '/')}>
-                List of Accounts 
-                </Link>
-            </li>
-
-            <li class="nav-item">
-              <Link className={this.isActiveTab('/page2')} to="/page2" onClick={event => this.onTabClick(event, '/page2')}>
-                List of Transactions
-                </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    )
-  }
+    render () {
+        return (
+            <ul className='nav page-tabs'>
+                <li className='nav-item'>
+                    <Link className={this.isActiveTab('/')} to="/"                //to attribute: what page it should go to
+                          onClick={event => this.onTabClick(event, '/')}>
+                        Accounts
+                    </Link>
+                </li>
+                <li className='nav-item'>
+                    <Link className={this.isActiveTab('/page2')} to="/page2"
+                          onClick={event => this.onTabClick(event, '/page2')}>
+                        Transactions
+                    </Link>
+                </li>
+                <li className='nav-item'>
+                    <Link className={this.isActiveTab('/page3')} to="/page3"
+                          onClick={event => this.onTabClick(event, '/page3')}>
+                        Add Account
+                    </Link>
+                </li>
+            </ul>
+        )
+    }
 
 };
 

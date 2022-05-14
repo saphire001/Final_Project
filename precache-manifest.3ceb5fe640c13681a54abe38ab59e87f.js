24096007c7bc299adac2ac523f0e963287e5d301
saanikajoshi <spj29@njit.edu>
2022-05-13 21:20:27 -0400

    Updates

---
+++ precache-manifest.3ceb5fe640c13681a54abe38ab59e87f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb80338987c468813221f56ee5316476",
+    "url": "/Final_Project/index.html"
+  },
+  {
+    "revision": "954f03426af1e01c83d1",
+    "url": "/Final_Project/static/css/2.5aa997f2.chunk.css"
+  },
+  {
+    "revision": "606f90c76ecab00bdbc7",
+    "url": "/Final_Project/static/css/main.425a839a.chunk.css"
+  },
+  {
+    "revision": "954f03426af1e01c83d1",
+    "url": "/Final_Project/static/js/2.d606c82d.chunk.js"
+  },
+  {
+    "revision": "fe6dd1a0788106a23e13a00fbb041af3",
+    "url": "/Final_Project/static/js/2.d606c82d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "606f90c76ecab00bdbc7",
+    "url": "/Final_Project/static/js/main.d5ce5b25.chunk.js"
+  },
+  {
+    "revision": "b77e13133ae3b14134f9",
+    "url": "/Final_Project/static/js/runtime-main.3572e6f1.js"
+  }
+]);
